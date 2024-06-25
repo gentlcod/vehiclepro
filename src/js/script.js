30448364ebb3c2380d9d2d7b2629b266114d3380
mr-olabi <your-email@example.com>
2024-06-25 22:54:15 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// preloader ( when loading the page or navigate to another section )
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", () => {
+    loader.style.display = "none";
+});
+
